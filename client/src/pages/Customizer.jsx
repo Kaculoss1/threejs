e9fb0c3b7c8aaf6309565d7358e4de8bdd57d91f
Kaculoss1 <Sanialhassan853@gmail.com>
2023-09-04 22:36:54 +0000

    repaired server

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -41,7 +41,7 @@ const Customizer = () => {
             setPrompt={setPrompt}
             generatingImg={generatingImg}
             handleSubmit={handleSubmit}
-          /> 
+          />
         );
 
       default:
@@ -55,7 +55,7 @@ const Customizer = () => {
       // call our backend to generate an ai image!
       setGeneratingImg(true);
 
-      const response = await fetch("https://three-ai-wjxx.onrender.com/api/v1/dalle", {
+      const response = await fetch("http://localhost:8080/api/v1/dalle", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt }),
